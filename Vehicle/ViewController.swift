9c01d1713597fa799cbc645ccfa130bba94f4df4
Giovanni Pires da Silva <giovanni@grupoezoom.com.br>
2018-07-01 16:17:31 -0300

    Add a blue box to the current position of camera

--- Vehicle/ViewController.swift
+++ Vehicle/ViewController.swift
@@ -62,8 +62,23 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
 
     }
+    
+    @IBAction func addCar(_ sender: Any) {
+        guard let pointOfView = sceneView.pointOfView else { return }
+        let transform = pointOfView.transform
+        let orientation = SCNVector3(-transform.m31,-transform.m32,-transform.m33)
+        let location = SCNVector3(transform.m41,transform.m42,transform.m43)
+        let currentPositionOfCamera = orientation + location
+        let box = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        box.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        box.position = currentPositionOfCamera
+        self.sceneView.scene.rootNode.addChildNode(box)
+    }
 }
 
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+}
 extension Int {
     
     var degreesToRadians: Double { return Double(self) * .pi/180}
